789692504208d70748a909d2f3402b518880cb5f
clux <analsandblaster@gmail.com>
2013-06-06 19:49:16 +0100

    reformat code and allow for custom query parameters (like the new default units=metric)

--- index.js
+++ index.js
@@ -1,44 +1,67 @@
-var xml = require('libxmljs'),
-    request = require('request')
+var xml = require('libxmljs')
+  , qs = require('querystring')
+  , request = require('request');
 
-var Client = exports.Client = function Client(appKey) {
-  this.appKey = appKey
+function Client(appKey, opts) {
+  var qp = this.qp = {};
+  qp.appid = appKey;
+  qp.units = 'metric';
+  // Not all formats are supported yet. Needs work in pod mappers below.
+  // http://products.wolframalpha.com/docs/WolframAlpha-API-Reference.pdf
+  qp.format = 'plaintext,image';
+
+  Object.keys(opts || {}).forEach(function (key) {
+    qp[key] = opts[key];
+  });
 }
 
-Client.prototype.query = function(input, cb) {
-  if(!this.appKey) {
-    return cb("Application key not set", null)
+Client.prototype.query = function (input, cb) {
+  if(!this.qp.appid) {
+    return cb(new Error("Application key not set"), null);
+  }
+  if (!input) {
+    return cb(null, []);
   }
 
-  var uri = 'http://api.wolframalpha.com/v2/query?input=' + encodeURIComponent(input) + '&primary=true&appid=' + this.appKey
+  // TODO: perhaps other attributes should be set on a per query basis as well
+  // at the moment everything but input is global
+  this.qp.input = input;
+  var uri = 'http://api.wolframalpha.com/v2/query?' + qs.stringify(this.qp);
 
-  request(uri, function(error, response, body) {
-    if(!error && response.statusCode == 200) {
-      var doc = xml.parseXml(body), root = doc.root()
+  if (opts.debug) {
+    request(uri).pipe(process.stdout);
+    return;
+  }
 
-      if(root.attr('error').value() != 'false') {
-        var message = root.get('//error/msg').text()
-        return cb(message, null)
-      } else {
-        var pods = root.find('pod').map(function(pod) {
-          var subpods = pod.find('subpod').map(function(node) {
-            return {
-              title: node.attr('title').value(),
-              value: node.get('plaintext').text(),
-              image: node.get('img').attr('src').value()
-            }
-          })
-          var primary = (pod.attr('primary') && pod.attr('primary').value()) == 'true'
-          return { subpods: subpods, primary: primary }
-        })
-       return cb(null, pods)
-      }
-    } else {
-      return cb(error, null)
+  request(uri, function (error, response, body) {
+    if (error || response.statusCode !== 200) {
+      return cb(error, null);
     }
-  })
-}
+
+    var doc = xml.parseXml(body);
+    var root = doc.root();
+
+    if (root.attr('error').value() !== 'false') {
+      var message = root.get('//error/msg').text();
+      return cb(message, null);
+    }
+
+    var pods = root.find('pod').map(function (pod) {
+      var subpods = pod.find('subpod').map(function (node) {
+        var obj = {};
+        obj.title = node.attr('title').value();
+        obj.value = node.get('plaintext').text();
+        if (node.get('img')) { // if format does not include image, this isnt set
+          obj.image = node.get('img').attr('src').value();
+        }
+        return obj;
+      });
+      return { subpods: subpods };
+    });
+    cb(null, pods);
+  });
+};
 
 exports.createClient = function(appKey) {
-  return new Client(appKey)
-}
+  return new Client(appKey);
+};
